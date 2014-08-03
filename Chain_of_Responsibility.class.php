a439947e54c14017e121b440da36973fdc470fda
yangqinjiang <15976543860@139.com>
2014-08-03 15:17:24 +0800

    使用职责链模式的处理

--- Chain_of_Responsibility.class.php
+++ Chain_of_Responsibility.class.php
@@ -1,35 +1,49 @@
 <?php
 
-// 不使用职责链模式的处理
+// 使用职责链模式的处理
 
 // Chain of Responsibility
-$lev=$_POST['jubao']+0;
+// $lev=$_POST['jubao']+0;
 
 class board{
-	public function process()
+	//权限值
+	protected $power =1;
+	//$top上级
+	protected $top='admin';
+	public function process($lev)
 	{
-		echo "版主删帖";
+		if($lev<=$this->power){
+			echo "版主删帖";
+		}else{
+			$top=new $this->top;
+			$top->process($lev);
+		}
 	}
 }
 class admin{
-	public function process()
+	protected $power =2;
+	protected $top='police';
+	public function process($lev)
 	{
-		echo "管理员封账号";
+		if($lev<=$this->power){
+			echo "管理员封账号";
+		}else{
+			$top=new $this->top;
+			$top->process($lev);
+		}
 	}
 }
 class police{
-	public function process()
+	protected $top=null;//没有上一级
+	protected $power;
+	public function process($lev)
 	{
 		echo "捉起来";
 	}
 }
-if($lev == 1){
-	$judge = new board();
-	$judge->process();
-}else if($lev==2){
-	$judge = new admin();
-	$judge->process();
-}else{
-	  $judge = new police();
-	$judge->process();
-}
+// 使用职责链模式的处理
+// $lev=$_POST['jubao']+0;
+$lev=rand(1,4);
+echo $lev;
+$judge=new board();
+$judge->process($lev);
